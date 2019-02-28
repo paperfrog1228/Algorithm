7581e780a8fc3b8ff989917eba2e3d90e4cf01d9
paperfrog <paperfrog@naver.com>
2019-02-28 20:38:27 +0900

    9498 시험 성적
    오늘 진짜 행복했어!!

---
+++ Baekjoon/9498_시험 성적.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+if(n>=90)
+printf("A\n");
+else if(n>=80)
+printf("B\n");
+else if(n>=70)
+printf("C\n");
+else if(n>=60)
+printf("D\n");
+else
+printf("F\n");
+return 0;
+}
