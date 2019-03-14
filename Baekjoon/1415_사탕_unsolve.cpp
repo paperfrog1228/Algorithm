4bd712353eb96d2d178bc5e6a84892278a24e958
paperfrog <paperfrog@naver.com>
2019-03-14 23:20:25 +0900

    2742 기찍 N
    오늘 2019 게임 콘텐츠 잡 커넥트 갔다 왔다.
    그리고 갓겜 타이탄 소울을 샀다

--- Baekjoon/1415_사탕_unsolve.cpp
+++ Baekjoon/1415_사탕_unsolve.cpp
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<map>
+#include<vector>
 using namespace std;
-long long dp[500001][51],ans=0;
+long long dp[500001],ans=0,zero=1;
 int main(){
-	int n,temp,prime[500001];
+	int n,temp;
+	bool prime[500001];
 	map<int,int> c;
+
 	for(int i= 2; i<=500000;i++)
-		prime[500001]=i;//50만까지 소수를 구해놓는게 옳은 일일까..
+		prime[i]=i;//50만까지 소수를 구해놓는게 옳은 일일까..
 	for(int i= 2; i<=710;i++){
 		if(prime[i]==0) continue;
 	for(int j = i+i;j<=500000;j+=i)
@@ -15,24 +18,35 @@ int main(){
 	scanf("%d",&n);
 	for(int i=1;i<=n;i++)
 	{scanf("%d",&temp);
+		if(temp==0)
+		{zero++;
+			continue;}
 	c[temp]++;}
-	
-	dp[0][0]=1;
-	
-	long long max=0;
-		for(int i=1;i<=n;i++){
-				max+=c[i]*i;
-		for(auto it = c.begin(); it != c.end(); it++){
-		for(int count=1;count<=(it->second);count++)
-			for(int money=0;money<=max;money++)
-				{dp[money+(it->first*count)][i]+=dp[money][i-1];
-					printf("max: %d  dp[%d][%d]:%lld  dp[%d][%d]:%lld  \n",max,money+it->first*count,i,dp[money+it->first*count][i],money,i-1,dp[money][i-1]);}}
-		}
-	for(int i=2;i<=500000;i++)
-		if(prime[i]==0)
-			ans+=dp[i][n];
 
-	printf("%lld",ans);
+	dp[0]=1;
+
+	long long max=0;
+		for(auto it = c.begin(); it != c.end(); it++){
+			for(int count=1;count<=it->second;count++){
+				max+=it->first;
+				printf("max: %d count : %d\n",max,count);
+					for(int money=max;money>=0;money--){
+					if(money+(it->first)*count>max)
+						continue;
+					dp[money+(it->first)*count]+=dp[money];
+						printf("dp[%d+%d]:%d   dp[%d]:%d \n",money,(it->first)*count,dp[money+(it->first)*count],money,dp[money]);
+						}
+		}
+		}
+
+		for(int i=2;i<=max;i++){
+		if(prime[i]!=0){
+			ans+=dp[i];
+		//	if(dp[i]!=0)
+		}
+				printf("dp[%d]:%d\n",i,dp[i]);
+		}
+	printf("%lld",ans*zero);
 	return 0;
 }
 
