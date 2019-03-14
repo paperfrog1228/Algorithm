ed57dc66473ba0c11c0756283d13dfd5cf47dc29
paperfrog <paperfrog@naver.com>
2019-03-15 01:50:44 +0900

    1415 사탕
    와 정답률 11%일만 하다 진짜.
    특히 난 항상 0에서 바텀업 하는데 이 문제는 최대값에서 내려오는 바텀업을 써야하는 것,
    가격 0원의 사탕 처리 등등 어려웠다

--- Baekjoon/1415_사탕.cpp
+++ Baekjoon/1415_사탕.cpp
@@ -7,14 +7,12 @@ int main(){
 	int n,temp;
 	bool prime[500001];
 	map<int,int> c;
-
 	for(int i= 2; i<=500000;i++)
 		prime[i]=i;//50만까지 소수를 구해놓는게 옳은 일일까..
 	for(int i= 2; i<=710;i++){
 		if(prime[i]==0) continue;
 	for(int j = i+i;j<=500000;j+=i)
 		prime[j]=0;}
-	
 	scanf("%d",&n);
 	for(int i=1;i<=n;i++)
 	{scanf("%d",&temp);
@@ -22,30 +20,22 @@ int main(){
 		{zero++;
 			continue;}
 	c[temp]++;}
-
 	dp[0]=1;
-
 	long long max=0;
 		for(auto it = c.begin(); it != c.end(); it++){
-			for(int count=1;count<=it->second;count++){
-				max+=it->first;
-				printf("max: %d count : %d\n",max,count);
+				max+=it->first*it->second;
 					for(int money=max;money>=0;money--){
-					if(money+(it->first)*count>max)
-						continue;
-					dp[money+(it->first)*count]+=dp[money];
-						printf("dp[%d+%d]:%d   dp[%d]:%d \n",money,(it->first)*count,dp[money+(it->first)*count],money,dp[money]);
+						for(int count=1;count<=it->second;count++){
+							if(money+(it->first)*count>500000)
+								continue;
+						dp[money+(it->first)*count]+=dp[money];
 						}
+		}	
 		}
-		}
-
-		for(int i=2;i<=max;i++){
-		if(prime[i]!=0){
+		for(int i=2;i<=max;i++)
+		if(prime[i]!=0)
 			ans+=dp[i];
-		//	if(dp[i]!=0)
-		}
-				printf("dp[%d]:%d\n",i,dp[i]);
-		}
+			
 	printf("%lld",ans*zero);
 	return 0;
 }
