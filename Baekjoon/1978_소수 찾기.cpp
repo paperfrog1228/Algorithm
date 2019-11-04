d5a0b404026119aa9b811a1aeb4a6453910e7a9c
PaperFrog <paperfrog@naver.com>
2019-11-04 15:42:27 +0900

    1978 소수 찾기
    과거에 내가 이걸 왜 틀렸을까??

--- Baekjoon/1978_소수 찾기.cpp
+++ Baekjoon/1978_소수 찾기.cpp
@@ -1,18 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-int n,prime[10001],a,ans=0;
-for(int i = 2; i<=100;i++)
-	prime[i]=i;
-for(int i = 2; i<=100;i++){
-	if(prime[i]==0) continue;
-	for(int j = i+i;j<=100;j+=i)
-		prime[j]=0;}
-scanf("%d",&n);
-for(int i=1;i<=n;i++){
-	scanf("%d",&a);
-	if(prime[a]!=0)
-		ans++;}
-printf("%d",ans);
-return 0 ;
-}
+  int n,a,ans=0;
+  int prime[1002];
+  for(int i=2;i<=1000;i++)
+  {
+    prime[i]=-1;
+  }
 
+  for(int i =2;i<=32;i++)
+  { 
+    int j=2;
+    while(i*j<=1000)
+    {
+      prime[i*j]=1;
+      j++;
+    }
+  }
+  scanf("%d",&n);
+  for(int i = 1;i<=n;i++)
+  {
+    scanf("%d",&a);
+    if(prime[a]==-1)
+    ans++;
+  }
+  printf("%d",ans);
+  return 0;
+}
