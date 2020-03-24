d5d27fd4e7e08b8b963595c49f1fab69a432694d
PaperFrog <paperfrog@naver.com>
2020-03-24 13:00:35 +0900

    15663 N과 M (9)
    아 이거 다 떄려박아서 체크하는거 아닌거 같은데 고민하면서 그렇게 풀다가 시간초과났다..
    set이라는 자료구조를 쓰면 통과가능함.. stl 공부 좀 해야겠는데

---
+++ Baekjoon/15663_N과 M (9).cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<algorithm>
+#include<set>
+#include<vector>
+using namespace std;
+int n,m;
+set<vector<int>> last_ans;
+int arr[12],ans[12],visit[12];
+void DFS(int i,int cnt)
+{   
+    visit[i]=1;
+    cnt++;
+    ans[cnt]=arr[i];
+    if(cnt==m){
+        vector<int> s;
+        for(int j=1;j<=m;j++)
+        s.push_back(ans[j]);
+        last_ans.insert(s);
+    }
+    for(int j=1;j<=n;j++){
+        if(visit[j])
+        continue;
+        DFS(j,cnt);
+    }
+    visit[i]=0;
+    cnt--;
+}
+int main(){
+    scanf("%d %d",&n,&m);
+    for(int i=1;i<=n;i++)
+        scanf("%d",&arr[i]);
+    sort(arr+1,arr+n+1);
+    for(int i=1;i<=n;i++)
+        DFS(i, 0);
+    for(auto i = last_ans.begin(); i!=last_ans.end();++i){
+        for(auto j=(*i).begin();j!=(*i).end();++j)
+            printf("%d ",*j);
+        printf("\n");
+    }
+} 
