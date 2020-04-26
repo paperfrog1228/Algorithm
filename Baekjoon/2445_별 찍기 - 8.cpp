1fffee9ed68ad2b6a2b89b13753f017ece7c14b6
PaperFrog <paperfrog@naver.com>
2020-04-26 20:49:36 +0900

    2445 별 찍기 - 8
    오늘 되는일 하나도 없다 진짜 오늘 되는게 하나도 없어 진짜 으아아아
    과제 내지 말라고 시발놈들아 진짜

---
+++ Baekjoon/2445_별 찍기 - 8.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int n;
+int main(){
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++)
+        printf("*");
+        for(int j=1;j<=2*n-2*i;j++)
+        printf(" ");
+        for(int j=1;j<=i;j++)
+        printf("*");
+        printf("\n");
+    }
+    for(int i=n-1;i>=1;i--){
+        for(int j=1;j<=i;j++)
+        printf("*");
+        for(int j=1;j<=2*n-2*i;j++)
+        printf(" ");
+        for(int j=1;j<=i;j++)
+        printf("*");
+        printf("\n");
+    }
+}
