c1a7cfabe01ce9ca61f160c2c7c0407d4db18a44
paperfrog <paperfrog@naver.com>
2021-04-21 23:50:30 +0900

    11719 그대로 출력하기 2
    으아아아아 neerc 죽여버리고 싶다!

---
+++ Baekjoon/11719_그대로 출력하기 2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s[100];
+int main() {
+	for (int i = 0; i < 100; i++)
+		getline(cin,s[i]);
+	for (int i = 0; i < 100; i++)
+		cout << s[i] << endl;;
+}
+
