1348ae4f413bb28e98995eb1192bec93bceb5267
paperfrog <paperfrog@naver.com>
2021-05-26 21:25:19 +0900

    2207 가위바위보
    와 미쳤냐고 이거 2-SAT배워도 첨에 음 이게 왜 2-SAT인지 싶었다 와 진짜 미쳤다... 와ㅏㅏ

---
+++ Baekjoon/2207_가위바위보.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define MAXN 10010
+using namespace std;
+vector<int> adj[MAXN*2];
+stack<int> st;
+int n,m,dfsn[MAXN*2],finished[MAXN*2],sccid[MAXN*2],cnt,scnt;
+int findSCC(int here){
+    int ret=dfsn[here]=++cnt;
+    st.push(here);
+    for(auto next:adj[here]){
+        if(!dfsn[next]) ret=min(ret,findSCC(next));
+        else if(!finished[next]) ret=min(ret,dfsn[next]);
+    }
+    if(ret==dfsn[here]){
+        scnt++;
+        while(1){
+            int t=st.top();st.pop();
+            finished[t]++;
+            sccid[t]=scnt;
+            if(t==here) break;
+        }
+    }
+    return ret;
+}
+int main(){
+    scanf("%d %d",&m,&n);
+    int a,b,na,nb;
+    while(m--){
+        scanf("%d %d",&a,&b);
+        if(a<0)a=(-a)*2+1,na=a-1;
+        else a=a*2,na=a+1;
+        if(b<0)b=(-b)*2+1,nb=b-1;
+        else b=b*2,nb=b+1;
+        adj[na].push_back(b);
+        adj[nb].push_back(a);
+    }
+    for(int i=2;i<=n*2+1;i++) if(!dfsn[i]) findSCC(i);
+    for(int i=1;i<=n;i++)
+        if(sccid[i*2]==sccid[i*2+1]){
+            printf("OTL");
+            return 0;
+        }
+    printf("^_^");
+}
