7143fb238f279710bad8f7a707fa8c3c7d260169
paperfrog <paperfrog@naver.com>
2021-06-02 21:19:46 +0900

    LIS
    ㅁㅊ lis 풀 줄 몰라서 답 봄ㅋㅋㅋ... 탑 다운으로 하니깐 헷갈리긴 하다

---
+++ Algospot/AOJ_LIS.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <algorithm>
+#include <memory.h>
+#include <vector>
+int t,n,dp[510],ans;
+std::vector<int> s;
+int go(int i){
+    int &ret=dp[i];
+    if(ret!=-1) return ret;
+    ret=1;
+    for(int j=i+1;j<n;j++)
+        if(s[i]<s[j]) ret=std::max(go(j)+1,ret);
+    return ret;
+}
+void sol(){
+    ans=0;
+    memset(dp,-1,sizeof(dp));
+    scanf("%d",&n);
+    s.clear();
+    s.resize(n);
+    for(int i=0;i<n;i++) scanf("%d",&s[i]);
+    for(int i=0;i<n;i++) ans=std::max(go(i),ans);
+    printf("%d\n",ans);
+}
+int main(){
+    scanf("%d",&t);
+    while(t--) sol();
+}
