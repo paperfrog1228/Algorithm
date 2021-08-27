748a025bf54e87e903a455cab8c0bd79028ab11f
paperfrog <paperfrog@naver.com>
2021-08-27 18:58:16 +0900

    9372 상근이의 여행
    ..? 트리 문제가 아니잖아 이러면.. dfs 돌려야하나 싶엇는데 이거 스패닝트리면 당연히 n-1아니냐 문제 출제 의도가 이게 맞다는데 난 개고민함..

---
+++ Baekjoon/9372_상근이의 여행.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int t,n,m,a;
+int main(){
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d %d",&n,&m);
+        while(m--) scanf("%d %d",&a,&a);
+        printf("%d\n",n-1);
+    }
+}
