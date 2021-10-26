753c1104ce3cc5eea23104274e8857d45317f094
paperfrog <paperfrog@naver.com>
2021-10-26 21:52:38 +0900

    23251 스물셋
    하.. 오늘 카카오 엔터프라이즈 수시 코테쳤는데, 진짜 힘들다.. 4문제 중 3.5솔함. 근데 210분이나 주더라 시간을. 3번은 dp같은데 아이디어가 안 나와서 bfs돌리고 버림.
    만약 떨어지면 2번 풀 때 노가다하다 너무 빡쳐서 정규식 복붙해왔는데.. 설마 이걸 부정행위라고 하진 않겠지..?

---
+++ Baekjoon/23251_스물셋.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
+vector<long long> v;
+int n;
+string s;
+int main(){
+    cin>>n;
+    while(n--){
+        cin>>s;
+        reverse(s.begin(),s.end());
+        if(s.size()<13)
+        v.push_back(stol(s));
+    }
+    sort(v.begin(),v.end());
+    for(auto s:v) cout<<s<<endl;
+#include<stdio.h>
+int t;
+long long x;
+int main(){
+    scanf("%d",&t);
+    while(t--) scanf("%lld",&x),printf("%lld\n",x*23);
+}}
