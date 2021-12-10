cd78383d1bd6827020bda9a4678753de4a137b4b
paperfrog <paperfrog@naver.com>
2021-12-10 20:46:36 +0900

    1598 꼬리를 무는 숫자 나열
    으아 드디어 금요일! 피곤하다 으으으ㅡㅇ ㅡ 술 마실거다 첫 출근의 한 주가 지나간다. 회고는 낼 한다.
    이 문제 //는 내나 정수로 떨어지게 하는 그거다

---
+++ Baekjoon/1598_꼬리를 무는 숫자 나열.py
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg949\cocoartf2578
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red255\green255\blue254;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c100000\c100000\c99608;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl420\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 a,b=map(int,input().split())\cb1 \
+\cb3 a-=1\cb1 \
+\cb3 b-=1\cb1 \
+\cb3 print(abs(a//4-b//4)+abs(a%4-b%4))\cb1 \
+}
