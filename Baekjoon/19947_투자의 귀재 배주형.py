9a61a852aa3d7c1bfc90dc0790149e7ab517de2c
paperfrog <paperfrog@naver.com>
2021-12-12 15:13:25 +0900

    19947 투자의 귀재 배주형
    오늘은 홍범이집 집들이를 하는 날이다. 신난다! 
    처음에 브루트포스로 풀고 있었는데 문법 너무 더러워지고 타입 때문에 검색 했는데
    다른 사람들 걍 dp로 풀길래 나도 걍 dp로 해결했다. 코드 엄청 깔끔해지네

---
+++ Baekjoon/19947_투자의 귀재 배주형.py
@@ -0,0 +1,9 @@
+h,y=map(int,input().split())
+dp=[0 for _ in range(y+10)]
+dp[0]=h
+for i in range(1, y+1):
+    if i>=5:dp[i]=max(dp[i-1]*1.05, dp[i-3]*1.2, dp[i-5]*1.35)
+    elif i>=3:dp[i]=max(dp[i-1]*1.05,dp[i-3]*1.2)
+    else:dp[i]=dp[i - 1] * 1.05
+    dp[i]=int(dp[i])
+print(int(dp[y]))
