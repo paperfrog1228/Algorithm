db2caaaee1e980523230acbaaededad51e5b9d27
paperfrog <paperfrog@naver.com>
2022-02-04 20:01:31 +0900

    1402 아무래도이문제는A번난이도인것같다
    ㅁㅊ... 이 문제 개고민했음.. 아 귀찮은데 소인수 분해 해야하나 이 생각 엄청하다가 혹시나 하고 답봤는데
    난 아무래도 그른거 같다..ㅋㅋㅋ..
    오늘 신입 연수 한다고 하루를 보냈다. 신입연수가 있다고 하면 일이 없을줄 알았는데 끝날 때 쯤 되니깐 다시 생겨있었다 흑흑

---
+++ Baekjoon/1402_아무래도이문제는A번난이도인것같다.kt
@@ -0,0 +1,9 @@
+import java.io.*
+
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var t = readLine().toInt()
+    repeat(t) {
+        readLine()
+        println("yes")
+    }
+}
