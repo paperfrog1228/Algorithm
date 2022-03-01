79a1081d16a4e4a605acc7c4f2274778bf8cff42
종이개구리 <paperfrog@naver.com>
2022-03-01 20:12:05 +0900

    21612 Boiling Water
    아 뭐. 운동하고 개피곤하다고 진짜

---
+++ Baekjoon/21612_Boiling Water.cpp
@@ -0,0 +1,20 @@
+{\rtf1\ansi\ansicpg949\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 #include<iostream>\
+int b,p;\
+int main()\{\
+    scanf("%d",&b);\
+    p=b*5-400;\
+    printf("%d\\n",p);\
+    if(p<100)\
+        printf("1");\
+    else if(p>100)\
+        printf("-1");\
+    else printf("0");\
+    \
+\}}
